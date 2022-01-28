d2115a51ba003a1afd5e8a0ecf8ef7ee8b7c9a5f
fayez-baig <coderninja16@gmail.com>
2022-01-29 02:23:10 +0530

    fix: bug fixed

--- template/src/app/pages/HomePage/HomePage.tsx
+++ template/src/app/pages/HomePage/HomePage.tsx
@@ -2,23 +2,25 @@ import { FC } from 'react';
 import { SvgIcon } from 'app/domains/Common/components/svg-icon';
 
 const HomePage: FC = () => (
-	<>
-	 <span className="text-2xl font-bold text-green-700">
-          &nbsp;Discord&nbsp;
-        </span>
-        server !
-      </p>
+    <>
+        <p className="w-3/4 mx-auto mb-4 text-center md:w-5/6 md:text-base text-light-mode-text dark:text-dark-mode-text text-word-spacing">
+            This is the summary of our roadmap activities. You just need a laptop and an internet
+            connection to get started. <br />
+            Join our
+            <span className="text-2xl font-bold text-green-700">&nbsp;Discord&nbsp;</span>
+            server !
+        </p>
 
-      <a
-        href="https://discord.gg/SMsZ9Hx4d5"
-        target="_blank"
-        className="flex items-center px-4 mx-auto bg-orange-300 rounded cursor-pointer hover:bg-lime-300 animate-zoomInOut"
-        aria-label="Join Discord"
-        rel="noreferrer"
-      >
-        <SvgIcon className="w-24 h-8" icon="DiscordIcon" />
-      </a>
-	</>
+        <a
+            href="https://discord.gg/SMsZ9Hx4d5"
+            target="_blank"
+            className="flex items-center px-4 mx-auto bg-orange-300 rounded cursor-pointer hover:bg-lime-300 animate-zoomInOut"
+            aria-label="Join Discord"
+            rel="noreferrer"
+        >
+            <SvgIcon className="w-24 h-8" icon="DiscordIcon" />
+        </a>
+    </>
 );
 
 export default HomePage;
