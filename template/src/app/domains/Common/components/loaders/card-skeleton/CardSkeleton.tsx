f48efbe93cdd1fec100418134abbfd722f8fa425
faran4engg <faran4engg@gmail.com>
2022-01-28 21:02:12 +0100

    fix: cleanup tailwind stuff

--- template/src/app/domains/Common/components/loaders/card-skeleton/CardSkeleton.tsx
+++ template/src/app/domains/Common/components/loaders/card-skeleton/CardSkeleton.tsx
@@ -1,17 +1,17 @@
-import { FC } from 'react';
+import { FC, ReactElement } from 'react';
 import SkeletonBody from './SkeletonBody';
 
 import { CardSkeletonProps } from './types';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const CardSkeleton: FC<CardSkeletonProps> = ({ noOfCards }): any =>
+const CardSkeleton: FC<CardSkeletonProps> = ({ noOfCards }): ReactElement =>
     Array(noOfCards)
         .fill('-')
         .map((_, i) => (
             <div
                 // eslint-disable-next-line react/no-array-index-key
                 key={i}
-                className="p-4 mx-auto w-full h-28 rounded-md shadow-md dark:bg-dark-mode bg-light-mode"
+                className="w-full p-4 mx-auto rounded-md shadow-md h-28 dark:bg-dark-mode bg-light-mode"
             >
                 <SkeletonBody />
             </div>
--- template/src/assets/icons/index.tsx
+++ template/src/assets/icons/index.tsx
@@ -2,7 +2,7 @@ import { ReactComponent as Error404Icon } from './error-404.svg';
 import { ReactComponent as MenuIcon } from './menu.svg';
 
 import { ReactComponent as HomeIcon } from './home.svg';
-import { ReactComponent as AreaIcon } from './area.svg';
+
 import { ReactComponent as NextGenDevLogo } from './NextGenDevLogo.svg';
 
-export { HomeIcon, Error404Icon, MenuIcon, AreaIcon, NextGenDevLogo };
+export { HomeIcon, Error404Icon, MenuIcon, NextGenDevLogo };
